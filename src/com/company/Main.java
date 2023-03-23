d873256a23efb545fee466600ff30977f30f2dfe
Tomasz Kaminski <tomasz77ka@gmail.com>
2023-03-23 08:43:33 +0100

    some

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
         GamePanel gamePanel = new GamePanel();
+        Collection
 
     }
 }
